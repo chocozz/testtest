8767fcc85caa9489724ad872a9bc1699d950f0d0
chocozz <kthanyarat.b@gmail.com>
2016-04-07 20:58:58 +0700

    awfFEFEEEFEFE

--- f2/Main.java
+++ f2/Main.java
@@ -18,3 +18,31 @@ public class Main{
 	rrrrrrrrrrrrhgfh
 	}
 }
+
+public static void main(String args[]){
+		JFrame f = new JFrame("5410110223");
+		
+		JPanel p = new JPanel();
+		p.setBackground(Color.BLACK);
+		f.add(p);
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(400, 650);
+		f.setVisible(true);		// show 	
+
+		test
+	rrrrrrrrrrrrhgfh
+	}public static void main(String args[]){
+		JFrame f = new JFrame("5410110223");
+		
+		JPanel p = new JPanel();
+		p.setBackground(Color.BLACK);
+		f.add(p);
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(400, 650);
+		f.setVisible(true);		// show 	
+
+		test
+	rrrrrrrrrrrrhgfh
+	}
